199041b2eff263329d62bffddc8b2ba38cc477bd
AndreRFBaT <andrerfbatista@gmail.com>
2025-07-29 17:27:54 -0300

    ENH: Start project changes

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,24 +1,30 @@
 alert("Boas vindas ao jogo do número secreto!");
-let numeroSecreto = 29;
+let valorMaximo = 100
+let numeroSecreto = parseInt(Math.random() * valorMaximo + 1); // Gera um número aleatório entre 1 e 10
 console.log("Número secreto:", numeroSecreto);
 let chute;
 let tentativas = 1;
 
 while (chute !== numeroSecreto && tentativas <= 3) {
-    chute = Number(prompt("Escolha um número entre 1 e 100"));
+    chute = Number(prompt(`Escolha um número entre 1 e ${valorMaximo}:`));
 
     if (isNaN(chute)) {
         alert("Digite um número válido!");
         continue;
     }
+    // 1. Verifica se a palavra tentativa deve ser plural ou singular
+    let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+
     if (chute === numeroSecreto) {
-        alert(`Isso aí! Você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
+        alert(`Isso aí! Você acertou o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}!`);
+        break;
     } else {
-        if (numeroSecreto > chute) {
-            alert("O número secreto é maior que o seu chute!");
-        } else {
-            alert("O número secreto é menor que o seu chute!");
-        }
+        // Aqui o chute já tem valor
+        let mensagem = numeroSecreto > chute
+            ? "O número secreto é maior que o seu chute!"
+            : "O número secreto é menor que o seu chute!";
+        alert(mensagem);
+
         tentativas++;
         if (tentativas <= 3) {
             alert(`Você errou! Tentativa número ${tentativas}.`);
@@ -26,6 +32,7 @@ while (chute !== numeroSecreto && tentativas <= 3) {
             alert("Você esgotou suas tentativas! O número secreto era " + numeroSecreto + ".");
             break;
         }
+
         console.log(`Tentativa número ${tentativas}: ${chute}`);
         console.log("Número secreto:", numeroSecreto);
     }
